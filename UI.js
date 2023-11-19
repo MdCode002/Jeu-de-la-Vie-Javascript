81a5dcee55152929cf9c72b3e79efc8dbda1e6d3
Mouhamed002 <dioufmouhamed002@gmail.com>
2023-11-19 15:50:42 +0000

    news version

---
+++ UI.js
@@ -0,0 +1,98 @@
+// Variables
+var sliderRow = document.getElementById("RangeRow");
+var sliderCol = document.getElementById("RangeCol");
+var outputRow = document.getElementById("row");
+var outputCol = document.getElementById("collum");  // Correction du nom de la variable
+let ModifSize = document.getElementById("ModifSize");
+let PlayBtn = document.getElementById('PlayBtn');
+let PauseBtn = document.getElementById('PauseBtn');
+let Clear = document.getElementById("Clear");
+let Aleatoire = document.getElementById("Aleatoire");
+let cols;  // Ajout d'un point-virgule à la fin de la déclaration de variable
+let RowClearValue = 30;
+let ColClearValue = 40;
+let isPaused = true;
+
+// Affiche le nombre de lignes et de colonnes dans les paramètres
+outputRow.innerHTML = sliderRow.value;
+outputCol.innerHTML = sliderCol.value;  // Correction du nom de la variable
+sliderRow.oninput = function() {
+    outputRow.innerHTML = this.value;
+}
+sliderCol.oninput = function() {
+    outputCol.innerHTML = this.value;  // Correction du nom de la variable
+}
+
+// Fonction pour effacer le terrain
+function ClearFunc() {
+    cols = document.querySelectorAll('.column');
+    cols.forEach(col => {
+        col.remove();
+    });
+
+    initializeGrid(ColClearValue, RowClearValue);
+    // On réinitialise la génération
+    nbrgen = 0;
+    gen.innerHTML = nbrgen;
+    if (!isPaused) {
+        pauseInterval();
+        isPaused = true;
+    }
+    console.log(cellsAlive);
+}
+
+// Modifie la taille du terrain quand on clique sur le bouton
+ModifSize.addEventListener('click', () => {
+    cellsAlive = [];
+    cols = document.querySelectorAll('.column');
+    cols.forEach(col => {
+        col.remove();
+    });
+    RowClearValue = sliderRow.value;
+    ColClearValue = sliderCol.value;
+    initializeGrid(ColClearValue, RowClearValue);
+})
+
+// Efface le terrain
+Clear.addEventListener('click', () => {
+    cellsAlive = [];
+    ClearFunc();
+})
+
+// Boutons pour mettre en pause et jouer
+PlayBtn.addEventListener('click', () => {
+    if (isPaused) {
+        startInterval();
+        isPaused = false;
+    }
+    console.log(cellsAlive);
+});
+
+PauseBtn.addEventListener('click', () => {
+    if (!isPaused) {
+        pauseInterval();
+        isPaused = true;
+    }
+    console.log(cellsAlive);
+    console.log(squares.length);
+});
+
+// Bouton de génération aléatoire
+Aleatoire.addEventListener('click', () => {
+    // Je vide le terrain
+    cellsAlive = [];
+    ClearFunc();
+    squares = document.querySelectorAll('.square');
+    for (let i = 0; i < squares.length;) {
+        // On vérifie si la cellule n'est pas déjà vivante
+        if (squares[i].style.background != 'blue') {
+            squares[i].style.background = 'blue';
+
+            if (!cellsAlive.includes(i) && squares[i].style.background === 'blue') {
+                cellsAlive.push(i);
+            }
+        }
+        // On change le pas en l'additionnant avec un nombre aléatoire
+        i += Math.floor(Math.random() * 3) + 1;
+    }
+});
--- script.js
+++ script.js
@@ -1,57 +1,188 @@
-function isHorizontalBottom() {
-    return (scrollableContainer.scrollLeft + scrollableContainer.clientWidth) >= content.scrollWidth;
-  }
-  function isVerticalBottom() {
-    return (scrollableContainer.scrollTop + scrollableContainer.clientHeight) >= content.scrollHeight;
+// Déclaration des variables
+const gridContent = document.getElementById('content');
+let columns = document.querySelectorAll('.column');
+let gen = document.querySelector('#gen');
+let squares;
+var cellsAlive = [];
+let cellsMaxLenght;
+let interval;
+let nbrgen = 0;
+
+// Initialiser la grille avec des valeurs par défaut
+const numColumns = 40;
+const numRows = 30;
+
+// Ajouter une nouvelle colonne à la grille
+function loadMoreColumn() {
+  const newColumn = document.createElement('span');
+  newColumn.className = 'column';
+  gridContent.appendChild(newColumn);
+  columns = document.querySelectorAll('.column');
 }
 
-  
-  function loadMoreColumn() {
-    const newColumn = document.createElement('span');
-    newColumn.className = "column";
-    content.appendChild(newColumn);
-  }
-  
-  function loadMoreRow(isNewColumn) {
-    let columns = document.querySelectorAll(".column");
-    let max = columns[0].childElementCount;
-  
-    columns.forEach(column => {
-        if (isNewColumn) {
-            if (column.childElementCount < max) {
-                let difRow = max - column.childElementCount;
-                for (let i = 0; i < difRow; i++) {
-                    console.log(column.childElementCount,max);
-                    let newRows = document.createElement('span');
-                    newRows.className = "square";
-                    column.appendChild(newRows);
-                }
-            }
-        }else{
-            let newRow = document.createElement('span');
-            newRow.className = "square";
-            column.appendChild(newRow);
+// Ajouter une nouvelle ligne à chaque colonne
+function loadMoreRow() {
+  // cellsMaxLenght représente la longueur d'une colonne, cette valeur sera utile plus tard pour des calculs sur la position des cellules
+  cellsMaxLenght = columns[0].childElementCount;
+  columns.forEach((column) => {
+    let newRow = document.createElement('span');
+    newRow.className = 'square';
+    column.appendChild(newRow);
+  });
+}
+
+// Associer un gestionnaire d'événements aux cellules pour ajouter ou supprimer des cellules vivantes au clic
+function CreateAliveCells(cellsAlive) {
+  // récupérer toutes les cases de la grille
+  squares = document.querySelectorAll('.square');
+  squares.forEach((square, index) => {
+    // Associer un gestionnaire d'événements à chaque cellule
+    square.addEventListener('click', () => {
+      if (square.style.background != 'blue') {
+        // Si la cellule n'est pas vivante, on la met en bleu et on la rajoute dans le tableau
+        square.style.background = 'blue';
+        if (!cellsAlive.includes(index) && squares[index].style.background === 'blue') {
+          cellsAlive.push(index);
+          console.log(cellsAlive);
+        }
+      } else {
+        // Si la cellule est déjà vivante, on la met en gris et on la supprime du tableau
+        square.style.background = 'gray';
+        let indexOfCell = cellsAlive.indexOf(index);
+        // Vérifier si l'index a été trouvé
+        if (indexOfCell !== -1) {
+          // Supprimer l'élément du tableau
+          cellsAlive.splice(indexOfCell, 1);
+        }
       }
     });
-  }
-  
-  const scrollableContainer = document.getElementById('container');
-  const content = document.getElementById('content');
-  
-  scrollableContainer.addEventListener('scroll', function () {
-    if (isHorizontalBottom()) {
-      loadMoreColumn();
-      loadMoreRow(true);
-    }
-    if(isVerticalBottom()){
-        loadMoreRow();
-    }
   });
-  
-  for (let i = 0; i < 100; i++) {
+}
+
+// Fonction pour Générer la grille
+function initializeGrid(col, row) {
+  for (let i = 0; i < col; i++) {
+    // Ajoute une colonne à chaque itération
     loadMoreColumn();
   }
-  for (let i = 0; i < 100; i++) {
+
+  for (let i = 0; i < row; i++) {
+    // Ajoute une ligne à chaque itération
     loadMoreRow();
   }
-  
+  // Instancie l'écouteur d'événement pour la création des cellules
+  CreateAliveCells(cellsAlive);
+}
+
+// Fonction pour vérifier si une cellule est en vie
+function isAlive(cell) {
+  return cellsAlive.includes(cell);
+}
+
+// Fonction qui calcule le nombre de voisins d'une cellule
+function neighbor(cell, cellsMaxLenght, i) {
+  let cellNotAlive = [];
+
+  // Fonction qui vérifie si un voisin est vivant ou non, si oui, elle itère une variable i qui correspond au nombre de voisins vivants / sinon, elle le met dans un tableau où il y a les voisins non vivants
+  function checkNeighbor(offset) {
+    const neighborCell = cell + offset;
+    if (isAlive(neighborCell)) {
+      i++;
+    } else {
+      if (squares.length > neighborCell && neighborCell >= 0) {
+        cellNotAlive.push(neighborCell);
+      }
+    }
+  }
+
+  // Utilise la fonction checkNeighbor pour vérifier si les 8 voisins de la cellule sont vivants en fonction de leur position
+  checkNeighbor(-1);
+  checkNeighbor(1);
+  checkNeighbor(cellsMaxLenght + 1);
+  checkNeighbor(-cellsMaxLenght - 1);
+  checkNeighbor(cellsMaxLenght + 2);
+  checkNeighbor(-cellsMaxLenght - 2);
+  checkNeighbor(-cellsMaxLenght);
+  checkNeighbor(cellsMaxLenght);
+
+  // Retourne le nombre de voisins i / et le tableau de voisins non vivants
+  return { i, cellNotAlive };
+}
+
+// Fonction pour commencer la boucle principale de l'algorithme
+function startInterval() {
+  interval = setInterval(() => {
+    // itère le nombre de générations
+
+    // Un tableau qui contient la position des cellules qui doivent mourir
+    let WillDiePos = [];
+    // Un tableau qui contient l'index des cellules qui doivent mourir
+    let WillDieIndex = [];
+    // Un tableau qui contient les cellules qui doivent naître
+    let WillBorn = [];
+
+    // Pour chaque cellule vivante
+    cellsAlive.forEach((cell, index) => {
+      // i correspond au nombre de voisins d'une cellule vivante
+      let i = 0;
+      // on re-vérifie si la cellule est vraiment vivante
+      if (squares[cell] && squares[cell].style.background == 'blue') {
+        // on appelle la fonction neighbor et on stocke le résultat dans result
+        const result = neighbor(cell, cellsMaxLenght, i);
+        // on récupère le nombre de cellules vivantes qui sont voisines de la cellule courante
+        i = result.i;
+        // on récupère l'index des cellules mortes qui sont voisines de la cellule
+        let cellsNotAlive = result.cellNotAlive;
+
+        // on parcourt les cellules voisines de la cellule courante qui sont mortes
+        cellsNotAlive.forEach((cellNotAlive) => {
+          // on vérifie si elle n'a pas déjà été ajoutée dans le tableau des cellules qui doivent naître
+          if (!WillBorn.includes(cellNotAlive)) {
+            let j = 0;
+            // on récupère le nombre de voisins vivants qu'elle a et on la stocke dans j
+            let resultNotAlive = neighbor(cellNotAlive, cellsMaxLenght, j);
+            j = resultNotAlive.i;
+            // si le nombre de voisins vivants qu'elle a est égal à 3, alors on la pousse dans le tableau des cellules qui doivent naître
+            if (j == 3 && !isAlive(cellNotAlive) && cellNotAlive >= 0) {
+              WillBorn.push(cellNotAlive);
+            }
+          }
+        });
+        // Si la cellule vivante courante a plus de 3 voisins vivants ou moins de 2 voisins vivants, on la met dans les tableaux des cellules qui doivent mourir
+        if (i > 3 || i < 2) {
+          WillDiePos.push(cell);
+          WillDieIndex.push(index);
+        }
+      }
+    });
+
+    // on reverse le tableau WillDieIndex à cause de problèmes d'index
+    WillDieIndex.reverse();
+    for (let i = 0; i < WillDiePos.length; i++) {
+      // on supprime chaque cellule qui est dans les tableaux WillDie en les enlevant du tableau cellsAlive et en les mettant en gris
+      squares[WillDiePos[i]].style.background = 'gray';
+      cellsAlive.splice(WillDieIndex[i], 1);
+    }
+    WillBorn.forEach((cell) => {
+      // on met chaque cellule qui est dans le tableau WillBorn dans le tableau cellsAlive et on les met en bleu
+      squares[cell].style.background = 'blue';
+      cellsAlive.push(cell);
+    });
+    // si il y a un changement on incrémente la génération / sinon on considère que la génération est stable
+    if (WillDieIndex.length > 0 || WillBorn.length > 0) {
+      nbrgen++;
+      gen.innerHTML = nbrgen;
+    }
+  }, 100);
+}
+
+// fonction de pause
+function pauseInterval() {
+  clearInterval(interval);
+}
+
+// Appel initial pour initialiser la grille
+initializeGrid(numColumns, numRows);
+
+// initial Generation
+gen.innerHTML = nbrgen;
